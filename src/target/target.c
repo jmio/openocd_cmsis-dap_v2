390c86a1cec2411e7e783fd52f4c73eb06cb69e0
mio <n.nishiyama@tehji.co.jp>
2021-08-11 10:52:29 +0900

    Add vexriscv support code from https://github.com/SpinalHDL/openocd_riscv

--- src/target/target.c
+++ src/target/target.c
@@ -110,6 +110,7 @@ extern struct target_type riscv_target;
 extern struct target_type mem_ap_target;
 extern struct target_type esirisc_target;
 extern struct target_type arcv2_target;
+extern struct target_type vexriscv_target;
 
 static struct target_type *target_types[] = {
 	&arm7tdmi_target,
@@ -148,6 +149,7 @@ static struct target_type *target_types[] = {
 	&arcv2_target,
 	&aarch64_target,
 	&mips_mips64_target,
+	&vexriscv_target,
 	NULL,
 };
 
